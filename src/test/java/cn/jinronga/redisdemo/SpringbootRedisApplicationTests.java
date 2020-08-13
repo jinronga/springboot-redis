f3f368d4805cbcefcffa87a3926842fc29592c96
jinronga <1460595002@qq.com>
2020-08-13 21:33:49 +0800

    提交springboot整合redis的Code

--- src/test/java/cn/jinronga/redisdemo/SpringbootRedisApplicationTests.java
+++ src/test/java/cn/jinronga/redisdemo/SpringbootRedisApplicationTests.java
@@ -17,6 +17,18 @@ class SpringbootRedisApplicationTests {
 
     @Test
     void contextLoads() {
+        // redisTemplate 操作不同的数据类型，api和我们的指令是一样的
+        // opsForValue 操作字符串 类似String
+        // opsForList 操作List 类似List
+        // opsForSet
+        // opsForHash
+        // opsForZSet
+        // opsForGeo
+        // opsForHyperLogLog
+        // 除了进本的操作，我们常用的方法都可以直接通过redisTemplate操作，比如事务，和基本的 CRUD
+        // 获取redis的连接对象
+        // RedisConnection connection = redisTemplate.getConnectionFactory().getConnection();
+        // connection.flushDb(); //
         redisTemplate.opsForValue().set("mykey","java--redis");
         System.out.println(redisTemplate.opsForValue().get("mykey"));
     }
